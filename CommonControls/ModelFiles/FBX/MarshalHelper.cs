24ab688a218ccbd6d84b71feade361b3868b63bb
mr-phazer <phazer1980@msn.com>
2023-07-11 00:29:10 +0200

    adfds

--- CommonControls/ModelFiles/FBX/MarshalHelper.cs
+++ CommonControls/ModelFiles/FBX/MarshalHelper.cs
@@ -1,11 +1,48 @@
-﻿using SharpDX.MediaFoundation.DirectX;
+﻿using Microsoft.Xna.Framework;
+using SharpDX.MediaFoundation.DirectX;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+using CommonControls.ModelFiles.FBX.
 
 namespace CommonControls.ModelFiles.FBX
 {
+    public abstract class IMarshalllerFancy<DEFINED_NATIVE_TYPE, MANAGED_TYPE>
+    {
+
+        public Dictionary<Type, IMarshalllerFancy<DEFINED_NATIVE_TYPE, MANAGED_TYPE>> dic = new Dictionary<Type, IMarshalller<DEFINED_NATIVE_TYPE, MANAGED_TYPE>>();;
+
+        public IMarshalllerFancy()
+        {
+            dic[typeof(DEFINED_NATIVE_TYPE)] = this;
+            _bIsBlitable = false;
+        }
+        public IMarshalllerFancy(bool isBlitable)
+        {
+            dic[typeof(DEFINED_NATIVE_TYPE)] = this;
+            _bIsBlitable = isBlitable;
+        }
+
+        public virtual MANAGED_TYPE FromNative(IntPtr nativePtr)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool _bIsBlitable = false;
+    }
+
+
+    public class DXVector3Marhaller : IMarshalllerFancy<XMFLOAT3, Vector3>
+    {
+        public Vector3 FromNative(IntPtr nativePtr)
+        {
+            Vector3? pVector3;
+            Marshal.Copy(pVector3, )
+        }
+    }
+
+
     abstract class IMarshaller<MANAGED_TYPE>
     {
         public virtual MANAGED_TYPE FromNative(IntPtr nativePtr)
