b279080c0ca58752942f9e59df9fe4bda9aeaa71
mr-phazer <phazer1980@msn.com>
2023-07-10 15:27:20 +0200

    Stuff

--- CommonControls/Editors/ModelImportSettings/ImportSettingsViewModel.xaml.cs
+++ CommonControls/Editors/ModelImportSettings/ImportSettingsViewModel.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace CommonControls.Editors
+namespace CommonControls.Editors.ModelImportSettings
 {
     /// <summary>
     /// Interaction logic for ImportSettings.xaml
